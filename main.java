60b3f8ab2b2b8036b902f62f83c286627e8b3c34
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-11-30 18:23:36 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,38 @@
-/*** PRIMITIVE TYPES ***/
+/*** PRIMITIVE TYPE ***/
 byte, short, long varName;
 int age = 35;
 double time = 1.2;
 boolean oldy = (age > 35);
 
-/*** STRINGS ***/
+/*** STRING ***/
 String name = "Name";
 System.out.println("Name: " + name);
 
-/*** TEST CONDITIONS ***/
+/*** ENUM ***/
+enum LootType {POTION, RING, ARMOR}
+
+/*** TEST CONDITION ***/
 if (age < 35) { 
 	System.out.println("Logic here");
 } else { System.out.println("Logic here"); }
 
-/*** CLASSES ***/
+/*** ARRAY ***/
+ArrayList<int> arrName = new ArrayList<>();		// initialize a new array list
+
+/*** LOOP ***/
+while (boolean_expression) {		// use break/continue
+	System.out.println("Logic here"); }
+
+do { System.out.println("Logic here"); 		// do-while
+	} while (boolean_expression);
+
+for(int i = 0; i < 5; i++) {		// for-in loop
+	System.out.println(i); }
+
+for (int item : arrayListName) {		// for-each loop
+	System.out.println(item); }
+
+/*** CLASS ***/
 public class ClassName {		// defining a class
 	private String handleName;		// class fields
 	private int startLives;
@@ -35,12 +54,17 @@ public class ClassName {		// defining a class
 		this.handleName = name;
 	}
 }
+public class SubClassName extends ClassName {
+	public SubClassName(String name, int lives) {		// call super class constructor
+		super(name, lives); }
+	@Override
+	public void methodName(int parameter) {		// override method
+		super.methodName(parameter / 2); }
+}
 ClassName name = new ClassName();		// create a new instance
 ClassName newName = new ClassName("Name");
 ClassName anotherName = new ClassName("Name", 5);
 
-/***  ***/
-/***  ***/
 /***  ***/
 /***  ***/
 /***  ***/
