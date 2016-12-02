598900995c809701672d3ec08c5d40b47a4a2c63
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-12-02 19:03:51 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,8 @@
+/*** LOGGING OUT DATA ***/
+private static final String TAG = "ClassName";
+Log.d(TAG, "methodName: information" + varName);
+Log.e(TAG, "methodName: error information");
+
 /*** PRIMITIVE TYPE ***/
 byte, short, long varName;
 int age = 35;
@@ -43,6 +48,7 @@ public class ClassName {		// defining a class
 	public ClassName(String name) {		// constructor with different signature
 		this(name, 3);
 	}
+
 	public ClassName(String name, int lives) {		// constructor with different signature
 		this.handleName = name;
 		this.startLives = lives;
@@ -72,205 +78,94 @@ ClassName anotherName = new ClassName("Name", 5);
 /***  ***/
 /***  ***/
 /***  ***/
-/***  ***/
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: starting async task");
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
-        Log.d(TAG, "onCreate: done.");
-    }
+/*** ANDROID SDK. BUTTONS AND FIELDS CONNECTION ***/
+private static final String STATE_PENDING_OPERATION = "PendingOperation";
+private static final String STATE_OPERAND_ONE = "OperandOne";
 
-    private class DownloadData extends AsyncTask<String, Void, String> {
-        private static final String TAG = "DownloadData";
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    result = (EditText) findViewById(R.id.result);
+    displayOperation = (TextView) findViewById(R.id.operation);
+    Button button = (Button) findViewById(R.id.button);
 
+    View.OnClickListener listener = new View.OnClickListener() {
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Log.d(TAG, "onPostExecute: parameter is " + s);
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            Log.d(TAG, "doInBackground: starts with " + strings[0]);
-            String rssFeed = downloadXML(strings[0]);
-            if (rssFeed == null)
-                Log.e(TAG, "doInBackground: Error downloading");
-            return rssFeed;
-        }
-
-        private String downloadXML (String urlPath) {
-            StringBuilder xmlResult = new StringBuilder();
-            try {
-                URL url = new URL(urlPath);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                int response = connection.getResponseCode();
-                Log.d(TAG, "downloadXML: The response code was: " + response);
-//                InputStream inputStream = connection.getInputStream();
-//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-//                BufferedReader reader = new BufferedReader(inputStreamReader);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                int charsRead;
-                char[] inputBuffer = new char[500];
-                while (true) {
-                    charsRead = reader.read(inputBuffer);
-                    if (charsRead < 0)
-                        break;
-                    if (charsRead > 0)
-                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
-                }
-                reader.close();
-                return xmlResult.toString();
-            } catch (MalformedURLException e) {
-                Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
-            } catch (IOException e) {
-                Log.e(TAG, "downloadXML: IO Exception reading data" + e.getMessage());
-            }
-            return null;
-        }
-    }
-
+        public void onClick(View view) {
+            Button b = (Button) view;
+            newNumber.append(b.getText().toString()); }
+    };
+    button.setOnClickListener(listener);
 }
 
+/*** ANDROID SDK. SAVE AND LOAD STATE OF DEVICE ***/
+@Override
+public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    outState.putString(STATE_PENDING_OPERATION, pendingOperation);
+    if (operandOne != null)
+        outState.putDouble(STATE_OPERAND_ONE, operandOne);
+    super.onSaveInstanceState(outState, outPersistentState);
+}
 
+@Override
+protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
+    operandOne = savedInstanceState.getDouble(STATE_OPERAND_ONE);
+    displayOperation.setText(pendingOperation);
+}
 
-
-
-
-public class MainActivity extends AppCompatActivity {
-    private EditText result;
-    private EditText newNumber;
-    private TextView displayOperation;
-
-    private Double operandOne = null;
-    private Double operandTwo = null;
-    private String pendingOperation = "=";
-
-    private static final String STATE_PENDING_OPERATION = "PendingOperation";
-    private static final String STATE_OPERAND_ONE = "OperandOne";
-
+/*** ANDROID SDK. ASYNC TASK ***/
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    DownloadData downloadData = new DownloadData();
+    downloadData.execute("http");
+}
+private class DownloadData extends AsyncTask<String, Void, String> {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        result = (EditText) findViewById(R.id.result);
-        newNumber = (EditText) findViewById(R.id.newNumber);
-        displayOperation = (TextView) findViewById(R.id.operation);
-
-        Button button0 = (Button) findViewById(R.id.button0);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
-        Button button7 = (Button) findViewById(R.id.button7);
-        Button button8 = (Button) findViewById(R.id.button8);
-        Button button9 = (Button) findViewById(R.id.button9);
-        Button buttonDot = (Button) findViewById(R.id.buttonDot);
-        Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
-        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
-        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
-        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button b = (Button) view;
-                newNumber.append(b.getText().toString());
+    protected void onPostExecute(String s) {
+    	super.onPostExecute(s); }
+    
+    @Override
+    protected String doInBackground(String... strings) {
+        String rssFeed = downloadXML(strings[0]);
+        if (rssFeed == null)
+            Log.e(TAG, "doInBackground: Error downloading");
+        return rssFeed; }
+    
+    private String downloadXML (String urlPath) {
+        StringBuilder xmlResult = new StringBuilder();
+        try {
+            URL url = new URL(urlPath);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            int response = connection.getResponseCode();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            int charsRead;
+            char[] inputBuffer = new char[500];
+            while (true) {
+                charsRead = reader.read(inputBuffer);
+                if (charsRead < 0)
+                    break;
+                if (charsRead > 0)
+                    xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
             }
-        };
-
-        button0.setOnClickListener(listener);
-        button1.setOnClickListener(listener);
-        button2.setOnClickListener(listener);
-        button3.setOnClickListener(listener);
-        button4.setOnClickListener(listener);
-        button5.setOnClickListener(listener);
-        button6.setOnClickListener(listener);
-        button7.setOnClickListener(listener);
-        button8.setOnClickListener(listener);
-        button9.setOnClickListener(listener);
-        buttonDot.setOnClickListener(listener);
-
-        View.OnClickListener opListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button b = (Button) view;
-                String op = b.getText().toString();
-                String value = newNumber.getText().toString();
-                try {
-                    Double doubleValue = Double.valueOf(value);
-                    performOperation(doubleValue, op);
-                } catch (NumberFormatException e) {
-                    newNumber.setText("");
-                }
-                pendingOperation = op;
-                displayOperation.setText(pendingOperation);
-            }
-        };
-        buttonEquals.setOnClickListener(opListener);
-        buttonDivide.setOnClickListener(opListener);
-        buttonMultiply.setOnClickListener(opListener);
-        buttonMinus.setOnClickListener(opListener);
-        buttonPlus.setOnClickListener(opListener);
-    }
-
-    @Override       // save state before device rotation starts
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        outState.putString(STATE_PENDING_OPERATION, pendingOperation);
-        if (operandOne != null)
-            outState.putDouble(STATE_OPERAND_ONE, operandOne);
-        super.onSaveInstanceState(outState, outPersistentState);
-    }
-
-    @Override       // load state after device rotation ends
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
-        operandOne = savedInstanceState.getDouble(STATE_OPERAND_ONE);
-        displayOperation.setText(pendingOperation);
-    }
-
-    private void performOperation(Double value, String operation) {
-        if (null == operandOne)
-            operandOne = value;
-        else {
-            operandTwo = value;
-            if (pendingOperation.equals("="))
-                pendingOperation = operation;
-            switch (pendingOperation) {
-                case "=":
-                    operandOne = operandTwo;
-                    break;
-                case "/":
-                    if (operandTwo == 0)
-                        operandOne = 0.0;
-                    else
-                        operandOne /= operandTwo;
-                    break;
-                case "*":
-                    operandOne *= operandTwo;
-                    break;
-                case "-":
-                    operandOne -= operandTwo;
-                    break;
-                case "+":
-                    operandOne += operandTwo;
-                    break;
-            }
+            reader.close();
+            return xmlResult.toString();
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "downloadXML: IO Exception reading data" + e.getMessage());
         }
-        result.setText(operandOne.toString());
-        newNumber.setText("");
+        return null;
     }
 }
+
+/*** ANDROID SDK. ***/
+
+/*** ANDROID SDK. ***/
+/*** ANDROID SDK. ***/
+/*** ANDROID SDK. ***/
+/*** ANDROID SDK. ***/
+/*** ANDROID SDK. ***/
