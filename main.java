0711e9c000bf1270c2b163ee3fdfdfed4cffe3cf
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-12-09 18:31:04 +0400

    Update main.java

--- main.java
+++ main.java
@@ -3,28 +3,63 @@ private static final String TAG = "ClassName";
 Log.d(TAG, "methodName: information" + varName);
 Log.e(TAG, "methodName: error information");
 
+
+/*** TYPE CASTING ***/
+varName = (byte) varNewName;
+
+
 /*** PRIMITIVE TYPE ***/
-byte, short, long varName;
-int age = 35;
-double time = 1.2;
+byte, short, int, long varName;     // width 8,16,32,64-bit
+char = 'a';     // width 16-bit
+float, double time = 1.2;       // width 32,64-bit
 boolean oldy = (age > 35);
 
-/*** STRING ***/
+
+/*** STRING OBJECT ***/
+// strings are not arrays of chars
 String name = "Name";
 System.out.println("Name: " + name);
 
+
 /*** ENUM ***/
 enum LootType {POTION, RING, ARMOR}
+
 
 /*** TEST CONDITION ***/
 if (age < 35) { 
 	System.out.println("Logic here");
 } else { System.out.println("Logic here"); }
 
+expressionOne ? expressionTwo : expressionThree;        // ternary operator
+
+
 /*** ARRAY ***/
+int arrName[] = new int[10];        // declaration and allocation of array
+int[] arrName = new int[10];        // declaration and allocation equivalent
+int[] arrOne, arrTwo, arrThree;     // several array declaration
+int arrOne[], arrTwo[], arrThree[];     // several array declaration equivalent
+int month[] = {31,28,31,30};        // a new array initializer
+
+int multi[][] = new int[2][3];      // alloc of array of arrays of int, 2 dimentions whith 3 values
+int[][] multi = new int[2][3];      // declaration and allocation equivalent
+int multi[][] = {{2,3},{3,4}};      // array of arrays initializer
+
 ArrayList<int> arrName = new ArrayList<>();		// initialize a new array list
 
-/*** LOOP ***/
+
+/*** CONTROL STATEMENT ***/
+if (condition) statement;
+if (condition) {
+    statement; statement;
+} else if (condition) statement;
+
+switch (expression) {       // switch statements can be nested
+    case valeOne:       // can be of type byte, short, int, char, String
+        statement; break;
+    default:
+        statement;
+}
+
 while (boolean_expression) {		// use break/continue
 	System.out.println("Logic here"); }
 
@@ -33,13 +68,24 @@ do { System.out.println("Logic here"); 		// do-while
 
 for(int i = 0; i < 5; i++) {		// for-in loop
 	System.out.println(i); }
+for (int i = 0, int x = 10; i < x; i++, x--) {
+    System.out.println(i); }
 
 for (int item : arrayListName) {		// for-each loop
 	System.out.println(item); }
+for (int x[]: ArrayList) {      // multidimentional array
+    for (int y : x) {
+        System.out.println("Logic here"); } }
+
+outer: for (;;) {       // named break and continue
+    for (;;) {
+        break outer;        // break will exit from inner loop, named break from outer loop
+    } }
+
 
 /*** CLASS ***/
 public class ClassName {		// defining a class
-	private String handleName;		// class fields
+	private String handleName;		// instance variables
 	private int startLives;
 	
 	public ClassName() {		// constructor
@@ -48,7 +94,6 @@ public class ClassName {		// defining a class
 	public ClassName(String name) {		// constructor with different signature
 		this(name, 3);
 	}
-
 	public ClassName(String name, int lives) {		// constructor with different signature
 		this.handleName = name;
 		this.startLives = lives;
@@ -59,6 +104,7 @@ public class ClassName {		// defining a class
 	public void setHandleName(String name) {		// setter method for every field
 		this.handleName = name;
 	}
+    protected void finalize() {}        // runtime will call this method before destroy the obj
 }
 public class SubClassName extends ClassName {
 	public SubClassName(String name, int lives) {		// call super class constructor
@@ -71,101 +117,6 @@ ClassName name = new ClassName();		// create a new instance
 ClassName newName = new ClassName("Name");
 ClassName anotherName = new ClassName("Name", 5);
 
+
 /***  ***/
 /***  ***/
-/***  ***/
-/***  ***/
-/***  ***/
-/***  ***/
-/***  ***/
-
-
-
-/*** ANDROID SDK. BUTTONS AND FIELDS CONNECTION ***/
-private static final String STATE_PENDING_OPERATION = "PendingOperation";
-private static final String STATE_OPERAND_ONE = "OperandOne";
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    result = (EditText) findViewById(R.id.result);
-    displayOperation = (TextView) findViewById(R.id.operation);
-    Button button = (Button) findViewById(R.id.button);
-
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Button b = (Button) view;
-            newNumber.append(b.getText().toString()); }
-    };
-    button.setOnClickListener(listener);
-}
-
-/*** ANDROID SDK. SAVE AND LOAD STATE OF DEVICE ***/
-@Override
-public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-    outState.putString(STATE_PENDING_OPERATION, pendingOperation);
-    if (operandOne != null)
-        outState.putDouble(STATE_OPERAND_ONE, operandOne);
-    super.onSaveInstanceState(outState, outPersistentState);
-}
-
-@Override
-protected void onRestoreInstanceState(Bundle savedInstanceState) {
-    super.onRestoreInstanceState(savedInstanceState);
-    pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
-    operandOne = savedInstanceState.getDouble(STATE_OPERAND_ONE);
-    displayOperation.setText(pendingOperation);
-}
-
-/*** ANDROID SDK. ASYNC TASK ***/
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    DownloadData downloadData = new DownloadData();
-    downloadData.execute("http");
-}
-private class DownloadData extends AsyncTask<String, Void, String> {
-    @Override
-    protected void onPostExecute(String s) {
-    	super.onPostExecute(s); }
-    
-    @Override
-    protected String doInBackground(String... strings) {
-        String rssFeed = downloadXML(strings[0]);
-        if (rssFeed == null)
-            Log.e(TAG, "doInBackground: Error downloading");
-        return rssFeed; }
-    
-    private String downloadXML (String urlPath) {
-        StringBuilder xmlResult = new StringBuilder();
-        try {
-            URL url = new URL(urlPath);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            int response = connection.getResponseCode();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            int charsRead;
-            char[] inputBuffer = new char[500];
-            while (true) {
-                charsRead = reader.read(inputBuffer);
-                if (charsRead < 0)
-                    break;
-                if (charsRead > 0)
-                    xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
-            }
-            reader.close();
-            return xmlResult.toString();
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
-        } catch (IOException e) {
-            Log.e(TAG, "downloadXML: IO Exception reading data" + e.getMessage());
-        }
-        return null;
-    }
-}
-
-/*** ANDROID SDK. ***/
-
-/*** ANDROID SDK. ***/
-/*** ANDROID SDK. ***/
-/*** ANDROID SDK. ***/
-/*** ANDROID SDK. ***/
-/*** ANDROID SDK. ***/
